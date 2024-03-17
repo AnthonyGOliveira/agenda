b1be973df8f19c0d6f4c3195855a14cc3e4b7919
AnthonyGOliveira <anthony.glivers@gmail.com>
2024-03-17 11:32:26 -0300

    correção de plural

--- contact/admin.py
+++ contact/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from contact import models
 
+
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
     list_display = 'id', 'first_name', 'last_name', 'phone', 'email',
@@ -11,3 +12,9 @@ class ContactAdmin(admin.ModelAdmin):
     list_max_show_all = 200
     # list_editable = 'first_name', 'last_name',
     list_display_links = 'id', 'phone',
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = '-id',
