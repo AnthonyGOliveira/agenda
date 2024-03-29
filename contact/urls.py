e79580c40843bc4cce722fb3723f904e62968fb1
AnthonyGOliveira <anthony.glivers@gmail.com>
2024-03-29 16:31:49 -0300

    pagination and search

--- contact/urls.py
+++ contact/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from contact import views
 
 urlpatterns = [
-    path('', view=views.index, name='index'),
-    path('<int:contact_id>/', view=views.contact, name='contact'),
+    path('contact/<int:contact_id>/detail/', views.contact, name='contact'),
+    path('contact/create', views.contact, name='contact'),
+    path('contact/<int:contact_id>/update/', views.contact, name='contact'),
+    path('contact/<int:contact_id>/delete/', views.contact, name='contact'),
+    path('search/', views.search, name='search'),
+    path('', views.index, name='index'),
 ]
