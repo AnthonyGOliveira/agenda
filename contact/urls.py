1af088a7af7985b36383c4467fcc440b463803d0
AnthonyGOliveira <anthony.glivers@gmail.com>
2024-03-29 14:55:16 -0300

    header

--- contact/urls.py
+++ contact/urls.py
@@ -3,4 +3,5 @@ from contact import views
 
 urlpatterns = [
     path('', view=views.index, name='index'),
+    path('<int:contact_id>/', view=views.contact, name='contact'),
 ]
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,12 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from contact.models import Contact
+from django.http import Http404
 
 # Create your views here.
 
 
 def index(request):
-    contacts = Contact.objects.all().order_by('-id')
+    contacts = Contact.objects.filter(show=True).order_by('-id')[:10]
     context = {
-        'contacts': contacts
+        'contacts': contacts,
+        'set_title': 'Contatos - '
     }
     return render(request, 'contact/index.html', context)
+
+
+def contact(request, contact_id):
+    # contact_single = Contact.objects.filter(pk=contact_id).first()
+    contact_single = get_object_or_404(
+        Contact.objects, pk=contact_id, show=True)
+
+    # if contact_single is None:
+    #     raise Http404()
+
+    context = {
+        'contact': contact_single,
+        'set_title': 'Detalhes de contato - '
+    }
+    return render(request, 'contact/contact.html', context)
