67a51283a8d48af0aa6211b073998448566538bf
AnthonyGOliveira <anthony.glivers@gmail.com>
2024-03-31 16:03:56 -0300

    django form contact

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -3,9 +3,29 @@ from contact.models import Contact
 from django.http import Http404
 from django.db.models import Q
 from django.core.paginator import Paginator
+from django import forms
 # Create your views here.
 
 
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = (
+            'first_name',
+            'last_name',
+            'phone',
+            'email',
+            'description'
+        )
+
+
 def create(request):
-    context = {}
+    context = {
+        'form': ContactForm()
+    }
+    if request.method == "POST":
+        context = {
+            'form': ContactForm(request.POST)
+        }
+
     return render(request, 'contact/create.html', context)
