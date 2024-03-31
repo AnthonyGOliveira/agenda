85321157da05cc063c479fb29c885b4e8c428fa8
AnthonyGOliveira <anthony.glivers@gmail.com>
2024-03-31 16:31:24 -0300

    field validation and hel text

--- contact/forms.py
+++ contact/forms.py
@@ -1,6 +1,8 @@
 # from django.core.exceptions import ValidationError
+from typing import Any
 from contact.models import Contact
 from django import forms
+
 
 class ContactForm(forms.ModelForm):
     class Meta:
@@ -19,3 +21,10 @@ class ContactForm(forms.ModelForm):
     #     # self.add_error('first_name', ValidationError(
     #     #     'Mensagem de erro teste', code='invalid'))
     #     return super().clean()
+
+    # def clean_first_name(self):
+    #     first_name = self.cleaned_data.get('first_name')
+    #     if first_name == 'ABC':
+    #         self.add_error('first_name', ValidationError(
+    #             'Campo não pode receber ABC', code='invalid'))
+    #     return first_name
