2b53534991b12ef7ad48e1a0ef148560cf084e30
AnthonyGOliveira <anthony.glivers@gmail.com>
2024-04-02 19:16:45 -0300

    user is autenticated?

--- contact/forms.py
+++ contact/forms.py
@@ -1,49 +1,61 @@
-from django.core.exceptions import ValidationError
-from typing import Any
-from contact.models import Contact
 from django import forms
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import password_validation
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
+from . import models
 
 
 class ContactForm(forms.ModelForm):
     picture = forms.ImageField(
         widget=forms.FileInput(
             attrs={
-                'accept': 'image/*'
+                'accept': 'image/*',
             }
-        )
+        ),
+        required=False
     )
 
     class Meta:
-        model = Contact
+        model = models.Contact
         fields = (
-            'first_name',
-            'last_name',
-            'phone',
-            'email',
-            'description',
-            'category',
-            'picture'
+            'first_name', 'last_name', 'phone',
+            'email', 'description', 'category',
+            'picture',
         )
 
-    # def clean(self):
-    #     cleaned_data = self.cleaned_data
-    #     # adding message validation error test
-    #     # self.add_error('first_name', ValidationError(
-    #     #     'Mensagem de erro teste', code='invalid'))
-    #     return super().clean()
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
 
-    # def clean_first_name(self):
-    #     first_name = self.cleaned_data.get('first_name')
-    #     if first_name == 'ABC':
-    #         self.add_error('first_name', ValidationError(
-    #             'Campo não pode receber ABC', code='invalid'))
-    #     return first_name
+        if first_name == last_name:
+            msg = ValidationError(
+                'Primeiro nome não pode ser igual ao segundo',
+                code='invalid'
+            )
+            self.add_error('first_name', msg)
+            self.add_error('last_name', msg)
+
+        return super().clean()
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+
+        if first_name == 'ABC':
+            self.add_error(
+                'first_name',
+                ValidationError(
+                    'Veio do add_error',
+                    code='invalid'
+                )
+            )
+
+        return first_name
+
 
 class RegisterForm(UserCreationForm):
-    ...
     first_name = forms.CharField(
         required=True,
         min_length=3,
@@ -71,7 +83,8 @@ class RegisterForm(UserCreationForm):
             )
 
         return email
-    
+
+
 class RegisterUpdateForm(forms.ModelForm):
     first_name = forms.CharField(
         min_length=2,
